993e12970604f16ba8b72683706ed64664b4282e
RidhuanDEV <ridhuankeren@gmail.com>
2023-09-22 16:06:18 +0700

    Update

--- Top up Web/login.js
+++ Top up Web/login.js
@@ -1,4 +1,4 @@
-
+// Variabel untuk mendapatkan ID di HTMLs
 const passwordInput = document.getElementById("password");
 const showPasswordCheckbox = document.getElementById("showPassword");
 
@@ -14,14 +14,15 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     event.preventDefault();
 
     // Ganti username dan password sesuai dengan yang Anda inginkan
-    const expectedUsername = "username";
-    const expectedPassword = "password";
+    const expectedUsername = "ridhuan";
+    const expectedPassword = "123";
 
     const usernameInput = document.getElementById("username").value;
     const passwordInputValue = passwordInput.value;
     
     if (usernameInput === expectedUsername && passwordInputValue === expectedPassword) {
         document.getElementById("message").innerHTML = "Login berhasil!";
+        location.href ="https://github.com/RidhuanDEV";
         // Di sini, Anda bisa mengarahkan pengguna ke halaman selanjutnya.
     } else {
         document.getElementById("message").innerHTML = "Login gagal. Silakan coba lagi.";
